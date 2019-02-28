f7651145c1e498c7c377f60b1c320986ec5a9a09
SedTheCursed <sed.robert@gmail.com>
2019-02-28 12:57:23 +0100

    Ajout du cinquième exercice

---
+++ exo_05.rb
@@ -0,0 +1,32 @@
+#Affiche un message
+puts "On va compter le nombre d'heures de travail à THP"
+#Les deux lignes suivantes affichent la concaténation d'un string et du produit d'une multiplication
+puts "Travail : #{10 * 5 * 11}"
+puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
+
+#Affiche un message
+puts "Et en secondes ?"
+
+#Affiche le produit d'une multiplication
+puts 10 * 5 * 11 * 60 * 60
+
+#Affiche un message
+puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
+
+#Affiche un booleen resultat d'une comparaison
+puts 3 + 2 < 5 - 7
+
+#Les deux lignes suivantes affichent la concaténation d'un string et respectivement de la somme d'une addition et de la difference d'une soustraction
+puts "Ça fait combien 3 + 2 ? #{3 + 2}"
+puts "Ça fait combien 5 - 7 ? #{5 - 7}"
+
+#Affiche un message
+puts "Ok, c'est faux alors !"
+
+#Affiche un message
+puts "C'est drôle ça, faisons-en plus :"
+
+#Les trois lignes suivantes affichent la concaténation d'un string et d'un booleen resultat d'une comparaison
+puts "Est-ce que 5 est plus grand que -2 ? #{5 > -2}"
+puts "Est-ce que 5 est supérieur ou égal à -2 ? #{5 >= -2}"
+puts "Est-ce que 5 est inférieur ou égal à -2 ? #{5 <= -2}"
