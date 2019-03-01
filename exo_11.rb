8b3159860643876225efc59f0115ce8336640347
SedTheCursed <sed.robert@gmail.com>
2019-03-01 11:15:13 +0100

    Ajout de l'exercice 11

---
+++ exo_11.rb
@@ -0,0 +1,12 @@
+#Écris un programme exo_11.rb qui demande un nombre à l'utilisateur, puis qui écrira autant de fois "Salut, ça farte ?"
+puts "Nombre de repetition souhaitée ?"
+print "> "
+repetition = gets.chomp.to_i
+
+if (repetition > 0)
+    repetition.times do |times|
+        puts "Salut, ça farte ?"
+    end
+else
+    puts "Vous avez entrer un nombre incorrect de répetitions."
+end
